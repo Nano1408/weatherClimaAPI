fb69971bedf8d317c570f95ba663994a3a03135a
Fernando Perez Florez <fernando.perez@gluky.local>
2022-11-16 17:32:26 -0500

    enter, estilos

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -6,28 +6,39 @@
 // console.log(clima)
 
 const send = document.querySelector("#send");
+const enter = document.querySelector("#city");
 const clima = document.querySelector("#clima");
+const viento = document.getElementById("viento");
 
 send.addEventListener("click",queryWheather);
-// city.addEventListener("enterchange",queryWheather)
+enter.addEventListener("keypress", enterTecla);
 
-function queryWheather(e){
+async function queryWheather(e){
     e.preventDefault();
-    const city = document.querySelector("#city").value;
+     const city = document.querySelector("#city").value;
     const key = 'bc53faa5060d584972f87db576cdc060&units=metric'
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    // const lang = "es"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=es`
     
     const weather = fetch(url)
     .then((response) => response.json())
     .then((data) => showIntoDOM(data))
 }
+ function enterTecla(e){
+     const teclapresionada = e.keyCode;
+     if(teclapresionada === 13){
+         document.getElementById("send").click()
+         e.preventDefault()
+     }
+ }
+
 function showIntoDOM(data){
     clearHTML()
-    const {name,main:{temp,humidity,feels_like}, weather} = data;
+    const {name,main:{temp,humidity,feels_like}, weather, wind:{speed, gust}} = data;
 
     const celsius = document.createElement("p");
     celsius.classList.add("celsius")
-    celsius.textContent = `La temperatura en ${name} es ${Math.ceil(temp)}º`
+    celsius.textContent = `${Math.ceil(temp)}ºC ${name}`
     clima.appendChild(celsius)
     const urlIcons = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
     const icon = document.createElement("img");
@@ -36,14 +47,23 @@ function showIntoDOM(data){
 
     const humidityData = document.createElement("p");
     humidityData.classList.add("celsius")
-    humidityData.textContent = `Su humedad es del ${humidity}% ${weather[0]["description"]}`
+    humidityData.textContent = `humedad: ${humidity}% ${weather[0]["description"]}`
     clima.appendChild(humidityData)
+
+    const velocidadViento = document.createElement("p");
+    velocidadViento.textContent = `Viento: a ${speed}m/s`
+    viento.appendChild(velocidadViento)
+
+    const velocidadRafaga = document.createElement("p");
+    velocidadRafaga.textContent = `Rafaga: a ${gust}m/s`
+    viento.appendChild(velocidadRafaga)
  console.log(data)
 }
 
 function clearHTML(){
     while(clima.firstChild){
         clima.removeChild(clima.firstChild)
+        viento.removeChild(viento.firstChild)
     }
 }
 
